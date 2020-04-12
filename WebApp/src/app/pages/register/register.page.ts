12690c938630594dbf49bf467cb3cec748db19df
Valério da Silva Martins <s.martins.valerio@gmail.com>
2020-04-12 18:30:56 -0300

    update validacoes

--- WebApp/src/app/pages/register/register.page.ts
+++ WebApp/src/app/pages/register/register.page.ts
@@ -13,3 +13,23 @@ export class RegisterPage implements OnInit {
   }
 
 }
+function validaForm(form) {
+  if(form.userName.value == "" || form.userName.value == null || form.userName.value.lenght < 3){
+    alert("Por favor, indique um nome!");
+    form.userName.focus();
+    return false;
+  }
+  if(form.lastName.value == "" || form.lastName.value == null || form.lastName.value.lenght < 3){
+    alert("Por favor, indique um nome!");
+    form.lastName.focus();
+    return false;
+  }
+  if(form.userEmail.value.indexOf("@") == -1 ||
+     form.userEmail.value.indexOf(".") == -1 ||
+     form.userEmail.value == "" || 
+     form.userEmail.valeu == null){
+       alert("Por favor, indique um e-mail válido.");
+       form.userEmail.focus();
+       return false;
+  }
+}
