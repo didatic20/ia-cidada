8b34b92d953e2931dad357ab5f51d16e864a009c
IagoRocha3112 <iagorocha1992@gmail.com>
2020-04-13 10:38:49 -0300

    ajustes

--- app/src/app/components/chat/chat.component.ts
+++ app/src/app/components/chat/chat.component.ts
@@ -8,12 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class ChatComponent implements OnInit {
 
   chatbot = {
-    answsers: [
-      { id: 'age', label: 'Idade', value: ''},
-      { id: 'gender', label: 'Sexo', value: ''},
-      { id: 'state', label: 'Estado', value: ''},
+    answsers: {
+      datas: [
+        { key: 'age', question: 'Idade', response: '' },
+        { key: 'gender', question: 'Sexo', response: '' },
+        { key: 'state', question: 'Estado', response: '' },
+        { key: 'city', question: 'Município', response: '' },
+        { key: 'district', question: 'Bairro', response: '' },
+      ],
+      symptoms: [
+        { question: 'Idade', response: ''},
+        { question: 'Sexo', response: ''},
+        { question: 'Estado', response: ''},
+      ],
+      comorbidities: [
+        { question: 'Diabetes', response: 0},
+        { question: 'Hipertensão arterial', response: 0},
+        { question: 'Insuficiência renal crônica', response: 0},
+        { question: 'Doença respiratória crônica', response: 0},
+        { question: 'Doença cardiovascular', response: 0},
+        { question: 'Câncer', response: 0},
+      ],
+      extras: [
+        { question: 'Você trabalha, mora ou tem contato frequente com alguém que trabalhe na área da saúde?', response: 0},
+        { question: 'Você mora/convive com pessoas que fazem parte do grupo de risco?', response: 0 },
+        { question: 'Você está em isolamento social?', response: 0 },
+        { question: ''}
+      ]
+
+
+    }
     ]
   };
+
   constructor() { }
 
   ngOnInit() {}
