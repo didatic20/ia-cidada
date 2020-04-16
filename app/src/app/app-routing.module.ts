951967290cb4a6482a12a256893325a9818d555c
IagoRocha3112 <iagorocha1992@gmail.com>
2020-04-16 20:22:44 -0300

    ticket #24

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
   {
     path: 'login',
