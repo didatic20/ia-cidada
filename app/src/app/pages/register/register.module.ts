3197d73e7f7f7c636c63613f3b565b3068d786a2
Valério da Silva Martins <s.martins.valerio@gmail.com>
2020-04-17 02:20:49 -0300

    Update
    Update page Login, Register, ForgotPassword.

--- app/src/app/pages/register/register.module.ts
+++ app/src/app/pages/register/register.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { RegisterPage } from './register.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    RegisterPageRoutingModule
+    RegisterPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [RegisterPage]
 })
